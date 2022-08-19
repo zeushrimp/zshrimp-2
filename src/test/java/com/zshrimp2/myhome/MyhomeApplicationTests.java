769c6080aa9f333db9dfce8a8f6d8bc11faf2b0e
JaewooShin <chung_103@naver.com>
2022-08-19 09:42:32 +0900

    20220819 zshrimp#2 by Zs

---
+++ src/test/java/com/zshrimp2/myhome/MyhomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zshrimp2.myhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyhomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
