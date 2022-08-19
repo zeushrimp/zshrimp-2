af0757b1aee2cf2539ad9202aa0a01f3205c341f
JaewooShin <chung_103@naver.com>
2022-08-19 11:07:59 +0900

    20220819 zshrimp#2 by Zs

---
+++ src/main/java/com/zshrimp2/myhome/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.zshrimp2.myhome.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping
+    public String index(){
+
+        return "index";
+    }
+}
